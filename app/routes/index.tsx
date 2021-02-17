a9d280ef904442467fd582ceab29b777eeec1f5c
benwis <ben@celcyon.com>
2021-02-16 21:07:44 -0800

    Basic example of mdx-bundler on vercel

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,21 @@
 import type {
   MetaFunction,
   LinksFunction,
-  LoaderFunction
+  LoaderFunction,
 } from "@remix-run/react";
 import { useRouteData } from "@remix-run/react";
 
 import styles from "url:../styles/index.css";
+import { GraphQLClient, gql } from "graphql-request";
+import { bundleMDX } from "mdx-bundler";
+import { MDXProvider } from "@mdx-js/react";
+import { getMDXComponent } from "mdx-bundler/client";
+import { json } from "@remix-run/data";
 
 export let meta: MetaFunction = () => {
   return {
     title: "Remix Starter",
-    description: "Welcome to remix!"
+    description: "Welcome to remix!",
   };
 };
 
@@ -18,12 +23,40 @@ export let links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
 
-export let loader: LoaderFunction = () => {
-  return { message: "this is awesome 😎" };
+export let loader: LoaderFunction = async () => {
+  const mdxSource = `
+---
+title: Example Post
+published: 2021-02-13
+description: This is some description
+---
+
+# Wahoo
+Here's a **neat** demo. Will it bundle on vercel?
+
+`.trim();
+
+  console.log(mdxSource);
+  const post = await bundleMDX(mdxSource);
+
+  return json(post, {
+    headers: {
+      "cache-control": `public, max-age=38400`,
+    },
+  });
 };
 
+function Post({ code, frontmatter }) {
+  const Component = getMDXComponent(code);
+  return (
+    <MDXProvider>
+      <Component />
+    </MDXProvider>
+  );
+}
 export default function Index() {
-  let data = useRouteData();
+  let post = useRouteData();
+  const { code, frontmatter } = post;
 
   return (
     <div style={{ textAlign: "center", padding: 20 }}>
@@ -32,7 +65,7 @@ export default function Index() {
         <a href="https://remix.run/dashboard/docs">Check out the docs</a> to get
         started.
       </p>
-      <p>Message from the loader: {data.message}</p>
+      <Post code={code} frontmatter={frontmatter} />
     </div>
   );
 }
